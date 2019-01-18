8878d1b2ff2fbaaaa895e5fb8e16709cf4409052
Rodrigo Ramos <darkar@Rodrigos-MacBook-Pro.local>
2019-01-17 19:27:02 -0500

    Initial Commit

---
+++ GLWF/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLWF
+//
+//  Created by Rodrigo Castro Ramos on 1/17/19.
+//  Copyright © 2019 Rodrigo Castro Ramos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
