c15f8588d04e480df2de31ef9f56fc6798d22ebd
Rodrigo Castro Ramos <darkar@Rodrigos-MacBook-Pro.local>
2019-01-30 12:13:57 -0500

    chepiur 3

---
+++ GLWF/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  GLWF
+//
+//  Created by Rodrigo Castro Ramos on 1/29/19.
+//  Copyright © 2019 Rodrigo Castro Ramos. All rights reserved.
+//
+
+#ifndef Player_h
+#define Player_h
+
+
+#endif /* Player_h */
