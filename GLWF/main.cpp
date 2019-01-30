d1a61fec748908ae50b53dafa0a7952d5f99ecbc
eyvind1 <eyvind1@github.com>
2019-01-30 12:22:19 -0500

    gg

--- GLWF/main.cpp
+++ GLWF/main.cpp
@@ -27,11 +27,13 @@
 #include "Light.h"
 #include "Coins.h"
 
+using namespace glm;
 
 // Function prototypes
 void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode );
 void MouseCallback( GLFWwindow *window, double xPos, double yPos );
 void DoMovement( );
+bool loadOBJ(const char* path,vector<vec3> & out_vertices,vector<vec2> & out_uvs,vector<vec3> & out_normals);
 
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
@@ -112,6 +114,10 @@ int main( )
     Shader lightingShader( "resources/shaders/lighting.vs", "resources/shaders/lighting.frag" );
     Shader lampShader( "resources/shaders/lamp.vs", "resources/shaders/lamp.frag" );
     
+    std::vector< glm::vec3 > vertices2;
+    std::vector< glm::vec2 > uvs;
+    std::vector< glm::vec3 > normals; // Won't be used at the moment.
+    bool res = loadOBJ("resources/obj/Lamborghini_Aventador.obj", vertices2, uvs, normals);
     // Set up vertex data (and buffer(s)) and attribute pointers
     GLfloat vertices[] =
     {
@@ -180,6 +186,7 @@ int main( )
         glm::vec3(  -4.0f,  2.0f, -12.0f    ),
         glm::vec3(  0.0f,  0.0f, -3.0f      )
     };
+
     
     // First, set the container's VAO (and VBO)
     GLuint VBO, boxVAO;
@@ -198,6 +205,24 @@ int main( )
     glEnableVertexAttribArray( 2 );
     glBindVertexArray( 0 );
     
+
+    GLuint carVBO,carVAO;
+    glGenVertexArrays(1,&carVAO);
+    glGenBuffers(1, &carVBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER,carVBO);
+    glBufferData(GL_ARRAY_BUFFER,vertices2.size()*sizeof(vec3),&vertices2[0],GL_STATIC_DRAW);
+
+    glBindVertexArray(carVAO);
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * )0 );
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * )( 3 * sizeof( GLfloat ) ) );
+    glEnableVertexAttribArray( 1 );
+    glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * )( 6 * sizeof( GLfloat ) ) );
+    glEnableVertexAttribArray( 2 );
+    glBindVertexArray(0);
+
+
     // Then, we set the light's VAO (VBO stays the same. After all, the vertices are the same for the light object (also a 3D cube))
     GLuint lightVAO;
     glGenVertexArrays( 1, &lightVAO );
@@ -254,7 +279,7 @@ int main( )
     
     //texturas de la moneda
     // Diffuse map
-    image = SOIL_load_image( "resources/images/coins.jpg", &imageWidth, &imageHeight, 0, SOIL_LOAD_RGB );
+    image = SOIL_load_image( "resources/images/image2.png", &imageWidth, &imageHeight, 0, SOIL_LOAD_RGB );
     glBindTexture( GL_TEXTURE_2D, diffuseMapCoin );
     glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image );
     glGenerateMipmap( GL_TEXTURE_2D );
@@ -265,7 +290,7 @@ int main( )
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST );
     
     // Specular map
-    image = SOIL_load_image( "resources/images/coins.jpg", &imageWidth, &imageHeight, 0, SOIL_LOAD_RGB );
+    image = SOIL_load_image( "resources/images/image2.png", &imageWidth, &imageHeight, 0, SOIL_LOAD_RGB );
     glBindTexture( GL_TEXTURE_2D, specularMapCoin );
     glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image );
     glGenerateMipmap( GL_TEXTURE_2D );
@@ -417,6 +442,10 @@ int main( )
         glBindVertexArray( 0 );
         
         
+        glBindVertexArray(carVAO);
+        
+        glBindVertexArray(0);
+
         // Dibujando las monedas
         // Bind diffuse map
         glUniformMatrix4fv( viewLoc, 1, GL_FALSE, glm::value_ptr( view ) );
@@ -559,4 +588,83 @@ void MouseCallback( GLFWwindow *window, double xPos, double yPos )
     lastY = yPos;
     
     camera.ProcessMouseMovement( xOffset, yOffset );
+}
+
+bool loadOBJ(const char* path,vector<vec3> & out_vertices,vector<vec2> & out_uvs,vector<vec3> & out_normals)
+{
+    vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+    vector<vec3> temp_vertices;
+    vector<vec2> temp_uvs;
+    vector<vec3> temp_normals;
+    FILE* file = fopen(path,"r");
+    if(file==NULL)
+    {
+        cout<<"no se puede leer"<<endl;
+        return false;
+    }
+    while( 1 )
+    {
+        char lineHeader[128];
+        // read the first word of the line
+        int res = fscanf(file, "%s", lineHeader);
+        if (res == EOF)
+            break;
+
+        if(strcmp(lineHeader,"v")==0)
+        {
+            vec3 vertex;
+            fscanf(file,"%f %f %f\n",&vertex.x,&vertex.y,&vertex.z);
+            temp_vertices.push_back(vertex);
+        }
+        else if (strcmp(lineHeader,"vt")==0)
+        {
+            vec2 uv;
+            fscanf(file,"%f %f\n",&uv.x,&uv.y);
+            temp_uvs.push_back(uv);
+        }
+        else if (strcmp(lineHeader,"vn")==0)
+        {
+            vec3 normal;
+            fscanf(file,"%f %f %f\n",&normal.x,&normal.y,&normal.z);
+            temp_normals.push_back(normal);
+        }
+        else if(strcmp(lineHeader,"f")==0)
+        {
+            string vertex1, vertex2,vertex3;
+            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+            int matches = fscanf(file,"%d/%d/%d %d/%d/%d %d/%d/%d\n",&vertexIndex[0],&uvIndex,&normalIndex);
+            vertexIndices.push_back(vertexIndex[0]);
+            vertexIndices.push_back(vertexIndex[1]);
+            vertexIndices.push_back(vertexIndex[2]);
+            uvIndices    .push_back(uvIndex[0]);
+            uvIndices    .push_back(uvIndex[1]);
+            uvIndices    .push_back(uvIndex[2]);
+            normalIndices.push_back(normalIndex[0]);
+            normalIndices.push_back(normalIndex[1]);
+            normalIndices.push_back(normalIndex[2]);  
+            for( unsigned int i=0; i<vertexIndices.size(); i++ )
+            {
+                unsigned int vertexIndex = vertexIndices[i];
+                vec3 vertex = temp_vertices[ vertexIndex-1 ];
+                out_vertices.push_back(vertex);
+
+            }
+            for( unsigned int i=0; i<uvIndices.size(); i++ )
+            {
+                unsigned int uvIndex = uvIndices[i];
+                vec2 uv = temp_uvs[ uvIndex-1 ];
+                out_uvs.push_back(uv);
+                
+            }
+            for( unsigned int i=0; i<normalIndices.size(); i++ )
+            {
+                unsigned int normalIndex = normalIndices[i];
+                vec3 normal = temp_normals[ normalIndex-1 ];
+                out_normals.push_back(normal);
+                
+            }    
+        }
+        
+    }
+
 }
