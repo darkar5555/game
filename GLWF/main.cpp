3ee46f29aa9b530a895f6b4520a4bd47fdbb1a1a
eyvind1 <eyvind1@github.com>
2019-01-30 19:43:55 -0500

    musica

--- GLWF/main.cpp
+++ GLWF/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-
+#include "SDL/SDL_mixer.h"
 // GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
@@ -277,6 +277,25 @@ int main( )
     
     glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( GLfloat )SCREEN_WIDTH / ( GLfloat )SCREEN_HEIGHT, 0.1f, 100.0f );
     
+    //MUisca del juego
+    Mix_Chunk *sound = NULL;
+    int channel;
+    int audio_rate = 22050;
+    Uint16 audio_format = AUDIO_S16SYS;
+    int audio_channels = 2;
+    int audio_buffers = 4096;
+    if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) != 0) {
+            printf("Unable to initialize audio: %s\n", Mix_GetError());
+                exit(1);
+    }
+    sound = Mix_LoadWAV("resources/music/Dystopic-Mayhem.wav");
+    if(sound == NULL) {
+            printf("Unable to load WAV file: %s\n", Mix_GetError());
+    }
+    channel = Mix_PlayChannel(-1, sound, -1);
+
+
+
     // Game loop
     while ( !glfwWindowShouldClose( window ) )
     {
